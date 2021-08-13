ab96abc681b270e5bde836e73cfbd27e985fa79e
Laraibi <meh.laraibi@gmail.com>
2021-08-13 17:33:32 +0100

    fixing heroku issues with database migrations

--- database/migrations/2021_08_04_164755_create_messages_table.php
+++ database/migrations/2021_08_04_164755_create_messages_table.php
@@ -14,6 +14,7 @@ class CreateMessagesTable extends Migration
     public function up()
     {
         Schema::create('messages', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->id();
             $table->integer('sender_id')->unsigned();
             $table->integer('receiver_id')->unsigned();
